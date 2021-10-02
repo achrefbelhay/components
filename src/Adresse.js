dba6d1e31714211c16865361c1da630d50346f91
achrefbelhay <achref771@hotmail.com>
2021-10-02 11:48:00 +0100

    first commit

---
+++ src/Adresse.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Adresse() {
+  return (
+    <div>
+      <h1>
+        8 Rue 10236 wardiya 2 Tunis <br /> email address: achref771@hotmail.com
+      </h1>
+    </div>
+  );
+}
+
+export default Adresse;
